418940df888508b697c6993eaac7fe9ab7c1e1a2
Seva Safris <seva@safris.org>
2014-11-09 15:56:52 +0700

    Removed all static imports, and unnecessary main() methods from tests.

--- xde/src/test/java/org/safris/xdb/xde/EntitiesTest.java
+++ xde/src/test/java/org/safris/xdb/xde/EntitiesTest.java
@@ -1,11 +1,10 @@
 package org.safris.xdb.xde;
 
-import static org.junit.Assert.assertEquals;
-
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.safris.commons.lang.Resources;
 import org.safris.xdb.xde.EntityBridgeUtil.Assignment;
@@ -25,28 +24,28 @@ public class EntitiesTest {
     in.close();
     final String sql = "SELECT patient.*, pr.given_name, pr.website, pr.*, pp.*, pp.id, pp.emergency_contact_name FROM provider pr, provider_patient pp, patient WHERE pr.gender = 'M' AND pr.id = pp.provider_id AND patient.id = pp.patient_id";
     final Map<String,Class<?>> aliasToBinding = EntityBridgeUtil.parseBindings(database, sql);
-    assertEquals(livecare_provider.class, aliasToBinding.get("pr"));
-    assertEquals(livecare_provider_patient.class, aliasToBinding.get("pp"));
-    assertEquals(livecare_patient.class, aliasToBinding.get("patient"));
-    
+    Assert.assertEquals(livecare_provider.class, aliasToBinding.get("pr"));
+    Assert.assertEquals(livecare_provider_patient.class, aliasToBinding.get("pp"));
+    Assert.assertEquals(livecare_patient.class, aliasToBinding.get("patient"));
+
     final EntityBridge entities = EntityBridge.getInstance(database);
     final List<Assignment> assignments = entities.parseAssignments(sql);
-    assertEquals(5, assignments.size());
+    Assert.assertEquals(5, assignments.size());
     Assignment assignment = assignments.get(0);
-    assertEquals(livecare_patient.class, assignment.binding);
-    assertEquals(17, assignment.entries.size());
+    Assert.assertEquals(livecare_patient.class, assignment.binding);
+    Assert.assertEquals(17, assignment.entries.size());
     assignment = assignments.get(1);
-    assertEquals(livecare_provider.class, assignment.binding);
-    assertEquals(2, assignment.entries.size());
+    Assert.assertEquals(livecare_provider.class, assignment.binding);
+    Assert.assertEquals(2, assignment.entries.size());
     assignment = assignments.get(2);
-    assertEquals(livecare_provider.class, assignment.binding);
-    assertEquals(42, assignment.entries.size());
+    Assert.assertEquals(livecare_provider.class, assignment.binding);
+    Assert.assertEquals(42, assignment.entries.size());
     assignment = assignments.get(3);
-    assertEquals(livecare_provider_patient.class, assignment.binding);
-    assertEquals(31, assignment.entries.size());
+    Assert.assertEquals(livecare_provider_patient.class, assignment.binding);
+    Assert.assertEquals(31, assignment.entries.size());
     assignment = assignments.get(4);
-    assertEquals(livecare_provider_patient.class, assignment.binding);
-    assertEquals(2, assignment.entries.size());
+    Assert.assertEquals(livecare_provider_patient.class, assignment.binding);
+    Assert.assertEquals(2, assignment.entries.size());
     // FIXME: Weak tests here! :| Should check the order of columns too
   }
 }
