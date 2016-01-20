df1f25d4b2c43b28a4e1e432b9b3ede869940baf
Seva Safris <seva@safris.org>
2016-01-19 17:37:25 -0800

    Added XDEException class.

--- xde/src/main/java/org/safris/xdb/xde/XDEException.java
+++ xde/src/main/java/org/safris/xdb/xde/XDEException.java
@@ -9,14 +9,15 @@ import org.safris.xdb.xdl.DBVendor;
 
 public class XDEException extends SQLException {
   private static final long serialVersionUID = 400839108529773414L;
+  private static final Map<DBVendor,Map<String,ErrorSpec>> errorSpecs = new HashMap<DBVendor,Map<String,ErrorSpec>>();
 
   public static class ErrorSpec {
     @SafeVarargs
     public ErrorSpec(final Pair<DBVendor,String> ... specs) {
       for (final Pair<DBVendor,String> spec : specs) {
-        Map<String,ErrorSpec> sqlCodeToException = XDEException.es.get(spec.a);
+        Map<String,ErrorSpec> sqlCodeToException = XDEException.errorSpecs.get(spec.a);
         if (sqlCodeToException == null)
-          XDEException.es.put(spec.a, sqlCodeToException = new HashMap<String,ErrorSpec>());
+          XDEException.errorSpecs.put(spec.a, sqlCodeToException = new HashMap<String,ErrorSpec>());
 
         sqlCodeToException.put(spec.b, this);
       }
@@ -24,20 +25,20 @@ public class XDEException extends SQLException {
   }
 
   public static final ErrorSpec UNIQUE_VIOLATION = new ErrorSpec(new Pair<DBVendor,String>(DBVendor.POSTGRE_SQL, "23505"));
+  public static final ErrorSpec NOT_NULL_VIOLATION = new ErrorSpec(new Pair<DBVendor,String>(DBVendor.POSTGRE_SQL, "23502"));
+
   public static final ErrorSpec UNIMPLEMENTED = new ErrorSpec();
 
-  private static final Map<DBVendor,Map<String,ErrorSpec>> es = new HashMap<DBVendor,Map<String,ErrorSpec>>();
-
   public static XDEException lookup(final SQLException e, final DBVendor vendor) {
-    Map<String,ErrorSpec> sqlCodeToException = es.get(vendor);
+    Map<String,ErrorSpec> sqlCodeToException = errorSpecs.get(vendor);
     if (sqlCodeToException == null) {
-      System.err.println("[WARNING] THE VENDOR " + vendor + " DOES NOT EXIST IN THE XDEException library!!!!!!!!!!!!!!! Add it!!!!!");
+      System.err.println("[WARNING] THE VENDOR " + vendor + " DOES NOT EXIST IN THE XDEException library!!!!!!!!!!!!!!! Add it!!!!! http://www.postgresql.org/docs/8.3/static/errcodes-appendix.html");
       return new XDEException(e, UNIMPLEMENTED);
     }
 
     final ErrorSpec errorSpec = sqlCodeToException.get(e.getSQLState());
     if (errorSpec == null) {
-      System.err.println("[WARNING] THE SQLSTATE " + e.getSQLState() + " FOR VENDOR " + vendor + " DOES NOT EXIST IN THE XDEException library!!!!!!!!!!!!!!! Add it!!!!!");
+      System.err.println("[WARNING] THE SQLSTATE " + e.getSQLState() + " FOR VENDOR " + vendor + " DOES NOT EXIST IN THE XDEException library!!!!!!!!!!!!!!! Add it!!!!! http://www.postgresql.org/docs/8.3/static/errcodes-appendix.html");
       return new XDEException(e, UNIMPLEMENTED);
     }
 
