15c70afad0cb32072cd7b69955492694e05454f5
Seva Safris <seva@safris.org>
2016-01-21 22:02:48 -0800

    Minor updates.

--- xde/src/main/java/org/safris/xdb/xde/XDEException.java
+++ xde/src/main/java/org/safris/xdb/xde/XDEException.java
@@ -26,6 +26,7 @@ public class XDEException extends SQLException {
 
   public static final ErrorSpec UNIQUE_VIOLATION = new ErrorSpec(new Pair<DBVendor,String>(DBVendor.POSTGRE_SQL, "23505"));
   public static final ErrorSpec NOT_NULL_VIOLATION = new ErrorSpec(new Pair<DBVendor,String>(DBVendor.POSTGRE_SQL, "23502"));
+  public static final ErrorSpec UNDEFINED_COLUMN = new ErrorSpec(new Pair<DBVendor,String>(DBVendor.POSTGRE_SQL, "42703"));
 
   public static final ErrorSpec UNIMPLEMENTED = new ErrorSpec();
 
