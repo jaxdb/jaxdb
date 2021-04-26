21b4d7f624cafb87719add9b1589f5eac3f65600
Seva Safris <seva@safris.org>
2021-04-26 16:05:13 +0700

    Fix reset_sequence.sql for missing sequence, re #18

--- ddlx/src/main/resources/org/jaxdb/oracle/reset_sequence.sql
+++ ddlx/src/main/resources/org/jaxdb/oracle/reset_sequence.sql
@@ -8,29 +8,47 @@ AS
   order_flag VARCHAR(1);
   cache_size NUMBER;
   last_number NUMBER;
-  currval NUMBER;
   diffval NUMBER;
 BEGIN
 
-  query := 'SELECT min_value, max_value, increment_by, cycle_flag, order_flag, cache_size, last_number FROM all_sequences WHERE sequence_name = ''' || UPPER(sequence_name) || '''';
-  -- DBMS_OUTPUT.PUT_LINE(query);
-  EXECUTE IMMEDIATE query INTO min_value, max_value, increment_by, cycle_flag, order_flag, cache_size, last_number;
-  -- DBMS_OUTPUT.PUT_LINE(min_value || ' ' || max_value || ' ' || increment_by || ' ' || cycle_flag || ' ' || order_flag || ' ' || cache_size || ' ' || last_number);
+  BEGIN
+
+    query := 'SELECT min_value, max_value, increment_by, cycle_flag, order_flag, cache_size, last_number FROM all_sequences WHERE sequence_name = ''' || sequence_name || '''';
+    -- DBMS_OUTPUT.PUT_LINE(query);
+    EXECUTE IMMEDIATE query INTO min_value, max_value, increment_by, cycle_flag, order_flag, cache_size, last_number;
+    -- DBMS_OUTPUT.PUT_LINE(min_value || ' ' || max_value || ' ' || increment_by || ' ' || cycle_flag || ' ' || order_flag || ' ' || cache_size || ' ' || last_number);
+
+  EXCEPTION WHEN no_data_found THEN
+
+    DECLARE
+      sequence_does_not_exist EXCEPTION;
+      PRAGMA exception_init(sequence_does_not_exist, -02289);
+    BEGIN
+
+      -- Get the current sequence value;
+      query := 'SELECT "' || sequence_name || '".NEXTVAL FROM dual';
+      -- DBMS_OUTPUT.PUT_LINE(query);
+      EXECUTE IMMEDIATE query;
+
+      query := 'SELECT min_value, max_value, increment_by, cycle_flag, order_flag, cache_size, last_number FROM all_sequences WHERE sequence_name = ''' || sequence_name || '''';
+      -- DBMS_OUTPUT.PUT_LINE(query);
+      EXECUTE IMMEDIATE query INTO min_value, max_value, increment_by, cycle_flag, order_flag, cache_size, last_number;
+      -- DBMS_OUTPUT.PUT_LINE(min_value || ' ' || max_value || ' ' || increment_by || ' ' || cycle_flag || ' ' || order_flag || ' ' || cache_size || ' ' || last_number);
+
+    EXCEPTION WHEN sequence_does_not_exist THEN
+      RETURN 0;
+
+    END;
+  END;
 
   IF start_with < min_value THEN
 
-    RAISE_APPLICATION_ERROR(-20001, 'sequence ' || sequence_name || '.NEXTVAL goes below MINVALUE');
+    RAISE_APPLICATION_ERROR(-20001, 'sequence "' || sequence_name || '".NEXTVAL goes below MINVALUE');
 
   ELSE
 
-    -- Get the current sequence value;
-    query := 'SELECT ' || sequence_name || '.NEXTVAL FROM dual';
-    -- DBMS_OUTPUT.PUT_LINE(query);
-    EXECUTE IMMEDIATE query INTO currval;
-    -- DBMS_OUTPUT.PUT_LINE(currval);
-
-    EXECUTE IMMEDIATE 'DROP SEQUENCE ' || sequence_name;
-    query := 'CREATE SEQUENCE ' || sequence_name || ' INCREMENT BY ' || increment_by || ' START WITH ' || start_with;
+    EXECUTE IMMEDIATE 'DROP SEQUENCE "' || sequence_name || '"';
+    query := 'CREATE SEQUENCE "' || sequence_name || '" INCREMENT BY ' || increment_by || ' START WITH ' || start_with;
 
     IF max_value IS NOT NULL THEN
       query := query || ' MAXVALUE ' || max_value;
@@ -69,8 +87,11 @@ BEGIN
 
   END IF;
 
-EXCEPTION
-  WHEN no_data_found THEN
-    RETURN 0;
-
 END reset_sequence;
+
+-- DROP SEQUENCE "sq_type_id";
+-- CREATE SEQUENCE "sq_type_id" INCREMENT BY 1 START WITH 1001 MAXVALUE 1000000 MINVALUE 0 CYCLE CACHE 1000000 NOORDER;
+
+-- EXEC DBMS_OUTPUT.PUT_LINE(reset_sequence('sq_type_id', 1001));
+-- SELECT "sq_type_id".CURRVAL FROM dual;
+-- SELECT "sq_type_id".NEXTVAL FROM dual;
